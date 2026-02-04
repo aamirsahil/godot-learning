8772aeddcfeace92d37f08edde5b3e0febf8bc89
Aamir Sahil <notifysahil@gmail.com>
2026-02-04 15:27:35 +0530

    rename scene and scripts

--- scripts/ui/main_menu.gd
+++ scripts/ui/main_menu.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _on_button_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/level/level1.tscn")
+	get_tree().change_scene_to_file("res://scenes/level/Level_1.tscn")
 
 func _on_button_quit_pressed() -> void:
 	get_tree().quit()
--- scripts/ui/menu.gd
+++ scripts/ui/menu.gd
@@ -1,27 +1,12 @@
 extends Control
 
-@onready var label      = $ColorRect/VBoxContainer/HBoxContainer/Label
-@onready var color_rect = $ColorRect
 @onready var logo       = $Logo
 @onready var music      = $AudioStreamPlayer
-@onready var menu_scene = "res://scenes/ui/game.tscn"
+@onready var menu_scene = "res://scenes/ui/Main_Menu.tscn"
 func _ready():
 	music.play()
 	# Initial states (safety)
-	label.modulate.a = 0.0
-	logo.modulate.a = 0.0
 	var tween = create_tween()
-
-	# Fade in label (5 seconds)
-	tween.tween_property(label, "modulate:a", 1.0, 5.0)
-
-	# Fade ColorRect to black (after label fade)
-	tween.tween_property(
-		color_rect,
-		"color",
-		Color.BLACK,
-		2.0
-	)
 	# Fade in logo
 	tween.tween_property(logo, "modulate:a", 1.0, 2.0)
 	# Wait for 3 seconds
